5d01fc28968c3d122ddbbdb1d7ed5996bf2dcb1f
Autono-Coder <autono-coder@bazztech.com>
2025-12-01 12:26:57 +0300

    Fix routing: switch from hash routing to history API

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch, Router } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
+
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
@@ -91,7 +91,7 @@ function App() {
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster />
-          <Router hook={useHashLocation}>
+          <Router>
             <RouterComponent />
           </Router>
         </TooltipProvider>
